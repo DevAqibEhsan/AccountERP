5105e239468d92d0574d1902217008fbd39dba04
DevAqibEhsan <ocpaqib@hotmail.com>
2023-01-01 16:50:22 +0500

    Changes

---
+++ AccountERPApp/wwwroot/js/Common.js
@@ -0,0 +1,20 @@
+﻿
+$(document).ready(function () {
+    $('#example').DataTable();
+});
+
+function FormShow() {
+    $("#grid").hide();
+    $("#form").show();
+}
+
+function GridShow() {
+    $("#form").hide();
+    $("#grid").show();
+}
+
+function RemoveSortingClass(ColumnId) {
+    if ($('#' + ColumnId).hasClass('sorting')) {
+        $('#' + ColumnId).removeClass("sorting");
+    }
+}
