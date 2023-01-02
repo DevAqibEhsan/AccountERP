757c9cc4e83abb4025d02b19961a303038eaa538
DevAqibEhsan <ocpaqib@hotmail.com>
2023-01-02 23:52:38 +0500

    Controller and Views added and CSS modified

---
+++ AccountERPApp/Controllers/SalesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountERPApp.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Delivery()
+        {
+            return View();
+        }
+
+        public IActionResult Invoice()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        public IActionResult Quotation()
+        {
+            return View();
+        }
+
+        public IActionResult ReceiveMoney()
+        {
+            return View();
+        }
+
+        public IActionResult RecuringInvoice()
+        {
+            return View();
+        }
+
+        public IActionResult Refund()
+        {
+            return View();
+        }
+
+        public IActionResult Return()
+        {
+            return View();
+        }
+
+        public IActionResult Settlement()
+        {
+            return View();
+        }
+    }
+}
